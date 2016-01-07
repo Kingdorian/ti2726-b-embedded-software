420ec96a02939d0db778921da99eb4e008b134d9
bvanwijk <b.vanwijk@student.tudelft.nl>
2016-01-07 13:19:19 +0100

    Tested version

--- arduino-project/robotbase_6/robotbase_6.ino
+++ arduino-project/robotbase_6/robotbase_6.ino
@@ -12,34 +12,42 @@
 #include <std_msgs/String.h>
 #include <geometry_msgs/Twist.h>
 #include <Ultrasonic.h>
+#include <math.h>
 
-//class NewHardware : public ArduinoHardware{
+class NewHardware : public ArduinoHardware{
   
-//public : NewHardware ( ) : ArduinoHardware(&Serial1,
-//57600) {};
-//};
+public : NewHardware ( ) : ArduinoHardware(&Serial1,
+  57600) {};
+};
 
 
-ros::NodeHandle nh;
-
+ros::NodeHandle_<NewHardware> nh;
 
 // the timer object
 SimpleTimer timer;
 int stopid;
+Ultrasonic ultrasonic(23,22);
 
 void messageCb( const geometry_msgs::Twist& robot_controls){
-  if(robot_controls.linear.x > 0){
-     moveForward();
-   } else if(robot_controls.angular.z < 0){
-     turnRight();
-   } else if(robot_controls.angular.z > 0){
-     turnLeft();
-   } else if(robot_controls.linear.x < 0){
-     moveBackward();
-   } else{
-     stopRobot();
-  }    
+    double vel = fabs(robot_controls.linear.x);
+    if(vel < fabs(robot_controls.angular.z)){
+      vel = fabs(robot_controls.angular.z);
+    }
   
+  if(ultrasonic.Ranging(CM) > 10){
+   if(robot_controls.linear.x > 0){
+      move(255*vel, 255*vel);
+     } else if(robot_controls.angular.z < 0){
+        move(255*vel, -255*vel);
+     } else if(robot_controls.angular.z > 0){
+       move(-255*vel, 255*vel);
+     }
+  }else{
+      move(0, 0);
+  }
+    if(robot_controls.linear.x < 0){
+       move(-255*vel, -255*vel);
+    }
   if(timer.isEnabled(stopid)) {  
     timer.restartTimer(stopid);
   } else{
@@ -49,72 +57,36 @@ void messageCb( const geometry_msgs::Twist& robot_controls){
 
 ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", &messageCb );
 
+void move(double leftmotor, double rightmotor){
+  //enable both motors
+  
+    // Enable motors
+    analogWrite(24, 255);
+    analogWrite(25, 255);
+    if(rightmotor > 0){
+      //Forward right motor
+      analogWrite(7, 0);
+      analogWrite(6, rightmotor);
+    }else{
+       //Backward right motor
+      analogWrite(7, fabs(rightmotor));
+      analogWrite(6, 0);  
+    }
+    if(leftmotor > 0){
+       //Forward left motor
+      analogWrite(2, fabs(leftmotor));
+      analogWrite(3, 0);
+    }else{
+       //Backward right motor
+      analogWrite(2, 0);
+      analogWrite(3, fabs(leftmotor));      
+    }  
+   
+ 
+}
+
 void stopRobot(){
-  //disable motors
-  analogWrite(24, 0); 
-  analogWrite(25, 0);
-  
-  //Make all the motors stop
-  analogWrite(3, 255);
-  analogWrite(7, 255);
-  analogWrite(6, 255);
-  analogWrite(2, 255);
-}
-
-void moveForward(){
-  //enable both motors
-  analogWrite(24, 255);
-  analogWrite(25, 255);
-  
-  //Forward right motor
-  analogWrite(7, 200);
-  analogWrite(6, 0);
-  
-  //Forward left motor
-  analogWrite(3, 200);
-  analogWrite(2, 0);
-}
-
-void turnRight(){
-  //enable both motors
-  analogWrite(24, 255);
-  analogWrite(25, 255);
-  
-  //Backward right motor
-  analogWrite(6, 100);
-  analogWrite(7, 0);
-  
-  //Forward left motor
-  analogWrite(3, 100);
-  analogWrite(2, 0);
-}
-
-void turnLeft(){  
-  //enable both motors
-  analogWrite(24, 255);
-  analogWrite(25, 255);
-  
-  //Forward left motor
-  analogWrite(6, 0);
-  analogWrite(7, 100);
-  
-  //Backward right motor
-  analogWrite(3, 0);
-  analogWrite(2, 100);
-}
-
-void  moveBackward(){
-  //enable both motors
-  analogWrite(24, 255);
-  analogWrite(25, 255);
-  
-  //Backward right motor
-  analogWrite(6, 100);
-  analogWrite(7, 0);
-  
-  //Backward right motor
-  analogWrite(3, 0);
-  analogWrite(2, 100);
+    move(0, 0);
 }
 
 void setup()
@@ -125,15 +97,9 @@ void setup()
   stopid = timer.setTimeout(500, stopRobot);
 }
 
-
 void loop()
 {
    nh.spinOnce();
    timer.run();
-   // check for obstacles
-  //byte wLeft = digitalRead(5);
- // byte wRight = digitalRead(7);
- // if(wLeft == 0 || wRight == 0){
- //    stopRobot();
-  //}
+   delay(10);
 }
--- linefollower_6.cpp
+++ linefollower_6.cpp
@@ -1,66 +1,58 @@
-#include <ros/ros.h>
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/image_encodings.h>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
 
-static const std::string OPENCV_WINDOW = "Image window";
-
-class ImageConverter
+/**
+ * This tutorial demonstrates simple receipt of messages over the ROS system.
+ */
+void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
-  ros::NodeHandle nh_;
-  image_transport::ImageTransport it_;
-  image_transport::Subscriber image_sub_;
-  image_transport::Publisher image_pub_;
-  
-public:
-  ImageConverter()
-    : it_(nh_)
-  {
-    // Subscrive to input video feed and publish output video feed
-    image_sub_ = it_.subscribe("/camera/image/compressed", 1, 
-      &ImageConverter::imageCb, this);
-    image_pub_ = it_.advertise("/image_converter/output_video", 1);
+  ROS_INFO("I heard: [%s]", msg->data.c_str());
+}
 
-    cv::namedWindow(OPENCV_WINDOW);
-  }
-
-  ~ImageConverter()
-  {
-    cv::destroyWindow(OPENCV_WINDOW);
-  }
-
-  void imageCb(const sensor_msgs::ImageConstPtr& msg)
-  {
-    cv_bridge::CvImagePtr cv_ptr;
-    try
-    {
-      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
-    }
-
-    // Draw an example circle on the video stream
-    if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-      cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
-
-    // Update GUI Window
-    cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-    cv::waitKey(3);
-    
-    // Output modified video stream
-    image_pub_.publish(cv_ptr->toImageMsg());
-  }
-};
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "image_converter");
-  ImageConverter ic;
+  /**
+   * The ros::init() function needs to see argc and argv so that it can perform
+   * any ROS arguments and name remapping that were provided at the command line.
+   * For programmatic remappings you can use a different version of init() which takes
+   * remappings directly, but for most command-line programs, passing argc and argv is
+   * the easiest way to do it.  The third argument to init() is the name of the node.
+   *
+   * You must call one of the versions of ros::init() before using any other
+   * part of the ROS system.
+   */
+  ros::init(argc, argv, "listener");
+
+  /**
+   * NodeHandle is the main access point to communications with the ROS system.
+   * The first NodeHandle constructed will fully initialize this node, and the last
+   * NodeHandle destructed will close down the node.
+   */
+  ros::NodeHandle n;
+
+  /**
+   * The subscribe() call is how you tell ROS that you want to receive messages
+   * on a given topic.  This invokes a call to the ROS
+   * master node, which keeps a registry of who is publishing and who
+   * is subscribing.  Messages are passed to a callback function, here
+   * called chatterCallback.  subscribe() returns a Subscriber object that you
+   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
+   * object go out of scope, this callback will automatically be unsubscribed from
+   * this topic.
+   *
+   * The second parameter to the subscribe() function is the size of the message
+   * queue.  If messages are arriving faster than they are being processed, this
+   * is the number of messages that will be buffered up before beginning to throw
+   * away the oldest ones.
+   */
+  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+
+  /**
+   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
+   * callbacks will be called from within this thread (the main one).  ros::spin()
+   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
+   */
   ros::spin();
+
   return 0;
 }
