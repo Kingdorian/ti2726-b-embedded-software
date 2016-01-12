76b90688a2933bf63c1d697d8731ced9c4fd0eb7
Bryan van Wijk <B.vanwijk@student.tudelft.nl>
2016-01-12 19:17:54 +0100

    Update robotbase_6.ino

--- arduino-project/robotbase_6/robotbase_6.ino
+++ arduino-project/robotbase_6/robotbase_6.ino
@@ -77,6 +77,7 @@ void messageCb( const geometry_msgs::Twist& robot_controls) {
 ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", &messageCb );
 
 //Move leftmotor and right motor between -255 and 255
+//Negative is opposite direction from positive
 void move(double leftmotor, double rightmotor){ 
   
     // Enable motors
