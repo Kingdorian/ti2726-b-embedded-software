486dc9482aed5017b4196ad6e4d673b2b97e7704
Bryan van Wijk <B.vanwijk@student.tudelft.nl>
2016-01-12 19:17:06 +0100

    Update robotbase_6.ino
    change comment

--- arduino-project/robotbase_6/robotbase_6.ino
+++ arduino-project/robotbase_6/robotbase_6.ino
@@ -76,7 +76,7 @@ void messageCb( const geometry_msgs::Twist& robot_controls) {
 //Subscribe to the cmd_vel topic
 ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", &messageCb );
 
-//Move leftmotor and right motor between 0 and 255
+//Move leftmotor and right motor between -255 and 255
 void move(double leftmotor, double rightmotor){ 
   
     // Enable motors
